366b74348677e85a7b826ea543113a2a5c060527
Taylor Clemons <22042685+tinfins@users.noreply.github.com>
2021-01-28 23:55:28 -0500

    Updated testPage to contain label and comboBox

--- big_teacher/src/gui/testPage.py
+++ big_teacher/src/gui/testPage.py
@@ -1,13 +1,25 @@
 import tkinter as tk
 from tkinter import ttk
 
-#from functools import partial
-LARGE_FONT = ("Verdana", 12)
+
 class Test_Page(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
         self.parent = parent
 
-        label = ttk.Label(self,text='Test Page',font=LARGE_FONT)
-        label.grid(row=1,column=0)
+        label = ttk.Label(self, text='Classes:')
+        classValue = tk.StringVar()
+        self.classSubject = ttk.Combobox(self, textvariable=classValue, state='readonly')
+
+        self.classSubject['values'] = ('CMSC 495-6380',
+                                  'SDEV 300-1234',
+                                  'SDEV 460-5678'
+                                  )
+
+        #self.grid_rowconfigure(1, weight=1, uniform='row')
+        self.grid_columnconfigure(0, weight=1, uniform='column')
+        self.grid_columnconfigure(1, weight=1, uniform='column')
+        self.grid_columnconfigure(2, weight=1, uniform='column')
+        label.grid(row=1,column=0, columnspan=2, padx=5, pady=25)
+        self.classSubject.grid(row=1,column=1, padx=35, pady=25)
